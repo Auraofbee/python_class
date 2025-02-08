bd6aa4d556ced5c964912ca68108376e57233902
Auraofbee <agapefdn@gmail.com>
2025-02-08 12:29:15 -0500

    my first commit

---
+++ day1_basics.py
@@ -0,0 +1,19 @@
+
+a =int(input("enter the first number:"))
+b =int(input("enter the 2nd number:"))
+
+# print(f"Addition:{a} + {b} ={a+b}")
+# print(f"division:{a} / {b} = {a/b}")
+# #string can concantenate wih string
+# #string cannot concantenate with int
+# x=a 
+# print(x)
+# x= x+b
+# print(x)
+# x /= b  # x/b
+# x *= b  # x * b
+# x -= b  # x - b
+# x += b  # x + b
+print(f"{a} == {b} : {a == b}")
+print (f"{a != b}") # True
+print(f"{a > b}") 
