d55936a2ed5cabc4f2f13a8885039f8fa6761edd
Auraofbee <agapefdn@gmail.com>
2025-02-08 23:25:13 -0500

    this is day 2

---
+++ day2_basics.py
@@ -0,0 +1,15 @@
+# var_1 = "welcome to python programming"
+# var_2 = " Let's learn together"
+
+# print(var_1)
+# print(var_2)  #print the output of the variables
+
+# name = "my full name is Benedicta Olotu"
+# age = " I'm 42 years old"
+# fav_pl = "my favorite programming language is python"
+
+# print(name, age, fav_pl)
+
+print(input("what is your name?"))
+print(input("what is your favorite color? "))
+print("Hello John, your favorite color is Blue")
---
+++ day2_variables.py
@@ -0,0 +1,32 @@
+# ###### Variable Declaration and Rules
+
+# name = "Alice"
+# age = 25
+# height = 5.6
+# is_student = True
+
+
+# print(name)
+# print(type(name)) #this is a string
+
+# print(age)
+# print(type(age)) #this is an integer
+
+
+# print(height)
+# print(type(height)) #this is a float
+
+
+# print(is_student)
+# print(type(is_student))   #this is a boolean
+
+
+################# Data types Practice
+movies = ["mufasa", "lion king", "Simba"]   # this is a list
+print(type(movies))
+
+subjects = ("maths", "statistics", "english")  #this is a tuple
+print(type(subjects))
+
+fruits = {"mango, orange, lemon"}     #this is a set
+print(type(fruits))
