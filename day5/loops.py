24528a8b8c33b43569c6557123d3ec2f0f09fd30
Benedicta Olotu <agapefdn@gmail.com>
2025-03-01 09:41:25 -0500

    loop added

---
+++ day5/loops.py
@@ -0,0 +1,39 @@
+#for looop
+
+# fruits = ["apple", "banana", "cherry", "mango", "pear", "orange"]
+
+# # for variable in sequence:
+# #   code...
+
+# for fruit in fruits:
+#     print(fruit)
+
+#     if fruit == "apple":
+#         print("cut it into half")
+
+#     if fruit =="mango":
+#         print("i want one more!") 
+
+#     print("out of loop")
+
+
+# word ="python"
+
+# for letter in word:
+#     print(letter)
+
+
+# nums = [10, 20, 30, 40, 50, 60]
+
+# duplicate =int(input("enter the number:"))
+
+# for num in nums:
+#     print(num)
+#     if num==duplicate:
+#         print("your number is in the list: ",num)
+#         break
+
+
+
+for i in range(10, 101, 15):
+    print(i)
